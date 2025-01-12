c8d18d67480c2e4e5555c3c30676c269ba22b603
hitesh <hites.297@gmail.com>
2025-01-12 14:34:22 +0530

    add video controls

--- src/Wedding/WeddingApp.js
+++ src/Wedding/WeddingApp.js
@@ -14,7 +14,7 @@ function Header() {
 
   useEffect(() => {
     const video = videoRef.current;
-    video.addEventListener("playing", () => setIsPlaying(true));
+    video.addEventListener("loadeddata", () => setIsPlaying(true));
     //video.addEventListener("canplay", () => setIsLoading(false));
   }, []);
 
