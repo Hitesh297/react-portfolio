3ef278e2ae1348be94d0b7e5b72bf2af697fa04f
hitesh <hites.297@gmail.com>
2025-01-12 20:10:24 +0530

    fix venue

--- src/Wedding/components/GoogleMap.js
+++ src/Wedding/components/GoogleMap.js
@@ -19,13 +19,12 @@ const GoogleMap = () => {
   };
 
   return (
-    <div style={mapContainerStyle}>
+    <div data-aos="zoom-in" style={mapContainerStyle}>
       <iframe
         title="Radhe Farm Navsari Map"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3726.9689836632033!2d72.92961257471131!3d20.913567491726113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be0f70039e8324f%3A0x33cbc093f8f12f03!2sRadhe%20Farm%20Navsari!5e0!3m2!1sen!2sin!4v1736593811605!5m2!1sen!2sin"
         style={iframeStyle}
         allowFullScreen={true}
-        loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
     </div>
