6033baadc191d00ad87d600271fb2b3d42236ee0
hitesh <hites.297@gmail.com>
2025-02-22 13:45:08 +0530

    updated images

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import PortfolioApp from "./portfolio/PortfolioApp";
 import WeddingApp from './Wedding/WeddingApp';
 import BlogApp from "./blog/BlogApp";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AllImages from "./Wedding/components/AllImages";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<PortfolioApp />} />
         <Route path="/blog" element={<BlogApp />} />
         <Route path="/wedding" element={<WeddingApp />} />
+        <Route path="/all-images" element={<AllImages />} />
       </Routes>
     </Router>
   )
---
+++ src/Wedding/components/AllImages.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import "./AllImages.css"; // Create AllImages.css
+
+const AllImages = () => {
+  const location = useLocation();
+  const { apiUrl, title } = location.state || {};
+  const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    if (apiUrl) {
+      const fetchImages = async () => {
+        try {
+          const response = await fetch(apiUrl);
+          const data = await response.json();
+          if (!response.ok) {
+            throw new Error(`Error fetching data: ${response.status}`);
+          }
+          const processedImages = data.map((image) => ({
+            name: image.name,
+            thumbnail: image.link.replace(".jpeg", "h.jpeg"),
+            fullSize: image.link.replace(".xxjpeg", "h.jpeg"),
+          }));
+          setImages(processedImages);
+        } catch (error) {
+          console.error("Error fetching images:", error);
+        }
+      };
+      fetchImages();
+    }
+  }, [apiUrl]);
+
+  const handleThumbnailClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const handleCloseFullscreen = () => {
+    setSelectedImage(null);
+  };
+
+  return (
+    <div className="all-images-container">
+      <h2 className="wedding-section-title">{title}</h2>
+      <div className="thumbnails-grid">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image.thumbnail}
+            alt={image.name}
+            className="thumbnail"
+            onClick={() => handleThumbnailClick(image)}
+          />
+        ))}
+      </div>
+      {selectedImage && (
+        <div className="fullscreen-overlay" onClick={handleCloseFullscreen}>
+          <div className="fullscreen-content">
+            <img src={selectedImage.fullSize} alt={selectedImage.name} className="fullscreen-image" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AllImages;
